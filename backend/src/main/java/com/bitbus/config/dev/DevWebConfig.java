445b0df9ee338dafb2ab59e9b04e26ad4e927d26
JayProck <jprochnow1@gmail.com>
2017-03-13 21:29:45 -0400

    Add CORs mapping with dev-web profile and provide eclipse run configuration

---
+++ backend/src/main/java/com/bitbus/config/dev/DevWebConfig.java
@@ -0,0 +1,23 @@
+package com.bitbus.config.dev;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@Profile("dev-web")
+public class DevWebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+}
